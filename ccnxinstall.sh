5dacce0fc6265bff63781bb944601c481c2418cd
Shakir James <shakir.james@gmail.com>
2012-01-25 20:26:22 -0600

    added install script

---
+++ ccnxinstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash 
+
+#
+# This script installs and configures CCNx applications on an EC2 host. 
+# It runs when an EC2 instances starts up; it's useful for automating 
+# the installation and onfiguration of your CCNx applications.
+#
+# Copyright 2011 Shakir James and Washington University in St. Louis.
+# See LICENSE for details.
+#
+
+echo $@
