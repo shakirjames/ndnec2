84f1d9cbf019d3e138b80400b0132d268f90b05c
John DeHart <jdd@wustl.edu>
2012-01-27 21:28:58 -0600

    Johns first commit of init script

--- ccnxinstall.sh
+++ ccnxinstall.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash 
 
+
 #
 # This script installs and configures CCNx applications on an EC2 host. 
 # It runs when an EC2 instances starts up; it's useful for automating 
@@ -10,3 +11,69 @@
 #
 
 echo $@
+
+################################
+
+wget http://www.arl.wustl.edu/~jdd/NDN/NDN_GEC.tar.gz
+
+   gunzip NDN_GEC.tar.gz
+   tar -xf NDN_GEC.tar
+   cd ~ ; NDN_GEC/ccnx-0.4.2/csrc/lib/ccn_initkeystore.sh 
+
+   sudo yum -y install make
+
+   sudo yum -y install openssl
+   sudo yum -y install gcc
+   sudo rm /lib/libcrypto.so.6
+   sudo yum -y install openssl-devel
+   sudo yum -y install expat-devel
+   sudo yum -y install libpcap-devel
+   sudo yum -y install java-1.6.0-openjdk-devel
+   #sudo yum -y install ant
+
+   # ANT
+   cd ~/NDN_GEC
+   gunzip apache-ant-1.8.2-bin.tar.gz
+   tar -xf apache-ant-1.8.2-bin.tar
+   sudo mkdir /usr/local/ant /usr/local/ant/bin /usr/local/ant/lib
+   sudo cp -p -R apache-ant-1.8.2/bin/* /usr/local/ant/bin
+   sudo cp -p -R apache-ant-1.8.2/lib/* /usr/local/ant/lib
+   export ANT_HOME=/usr/local/ant
+   export PATH=$PATH:$ANT_HOME/bin
+   echo "PATH=$PATH"
+
+   cd ~/NDN_GEC/ccnx-0.4.2
+   make clean
+   make all
+   sudo -E make install
+
+   cd ~/NDN_GEC/ccnx-dhcp
+   make clean
+   make 
+
+   cd ~/NDN_GEC/ccnping
+   make clean
+   make 
+
+   cd ~
+   echo "#!/bin/sh" > restartCCNX.sh
+   echo "export CCND_LOG=/var/log/ccnd.log" >> restartCCNX.sh
+   echo "CCNX_DEBUG=7" >> restartCCNX.sh
+   echo "sudo -E /usr/local/bin/ccndstop" >> restartCCNX.sh
+   echo "sudo -E /usr/local/bin/ccndstart" >> restartCCNX.sh
+   chmod 755 restartCCNX.sh
+
+   echo "#!/bin/sh" > chat_thru_washu.sh
+   echo "export CCNX_USER_NAME=chat_wu" >> chat_thru_washu.sh
+   echo "/usr/local/bin/ccndc add ccnx:/ tcp 128.252.153.193 9695" >> chat_thru_washu.sh
+   echo "ccnchat -text ccnx:/ndn/wustl.edu/tstchat100" >> chat_thru_washu.sh
+   chmod 755 chat_thru_washu.sh
+
+   echo "#!/bin/sh" > chat_thru_csu.sh
+   echo "export CCNX_USER_NAME=chat_csu" >> chat_thru_csu.sh
+   echo "/usr/local/bin/ccndc add ccnx:/ tcp 129.82.138.48 9695" >> chat_thru_csu.sh
+   echo "ccnchat -text ccnx:/ndn/wustl.edu/tstchat100" >> chat_thru_csu.sh
+   chmod 755 chat_thru_csu.sh
+
+   echo "#!/bin/sh" > home_washu.sh
+   echo "~/NDN_GEC/ccnx-dhcp/ccndhcpnode -f ~/NDN_GEC/ccnx-dhcp/ccn_dhcp_client.conf.WASHU " >> home_washu.sh
