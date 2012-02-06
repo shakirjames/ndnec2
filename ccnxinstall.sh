4b89471909d27b2f0cc94abec86039f7c48b9e60
John DeHart <jdd@wustl.edu>
2012-02-06 11:31:34 -0600

    different installs for Ubuntu vs. Amazon Linux

--- ccnxinstall.sh
+++ ccnxinstall.sh
@@ -35,9 +35,12 @@ wget http://www.arl.wustl.edu/~jdd/NDN/NDN_GEC.tar.gz
    if [ $APP_NAME = "vlc" ]
    then
       echo "VLC:"
-      apt-get update
-      apt-get install gcc
-      apt-get install openjdk-6-jre
+      apt-get -y update
+      apt-get -y install gcc
+      apt-get -y install openjdk-6-jre
+      apt-get -y install libssl-dev
+      apt-get -y install libexpat-dev
+      apt-get -y install libpcap-dev
    else
      yum -y install make
 
