b75bff4949b5a3d885b1211e6b8b6933c5951738
John DeHart <jdd@wustl.edu>
2012-03-02 13:46:12 -0600

    fix startApp for vlc

--- ccnxinstall.sh
+++ ccnxinstall.sh
@@ -394,7 +394,7 @@ case "$APP_NAME" in
 
     echo "#!/bin/bash" > /home/ubuntu/startApp.sh
     echo ""          >> /home/ubuntu/startApp.sh
-    echo 'if [ $# -ne 1 ] ' >> /home/ubuntu/startApp.sh
+    echo 'if [ $# -eq 1 ] ' >> /home/ubuntu/startApp.sh
     echo "then" >> /home/ubuntu/startApp.sh
     echo '  videofile=$1' >> /home/ubuntu/startApp.sh
     echo "else" >> /home/ubuntu/startApp.sh
